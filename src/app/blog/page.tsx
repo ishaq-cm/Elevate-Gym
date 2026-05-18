f0b5a6bebe58089af7bdbd8e7e4395e310c50008
Ishaq Ansari <ansariishaq6737@gmail.com>
2026-05-18 19:49:37 +0000

    update

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,4 @@
-import { client } from '@/lib/sanity'
+import { client } from '@/sanity/lib/client'
 
 export default async function BlogPage() {
   const posts = await client.fetch(`*[_type == "post"] | order(publishedAt desc) {
